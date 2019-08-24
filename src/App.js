d4b9908f619f342dff3a06a287e494fe09ee88b8
dislersd <dislersd@gmail.com>
2019-08-23 21:45:11 -0700

    linting config, starting to use jsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import { join } from "path";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Bandit", animal: "Dog", breed: "Lab" }),
-    React.createElement(Pet, {
-      name: "Pogo",
-      animal: "Dog",
-      breed: "Dalmation"
-    }),
-    React.createElement(Pet, {
-      name: "Isabel",
-      animal: "Cat",
-      breed: "Persian"
-    })
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, { name: "Bandit", animal: "Dog", breed: "Lab" }),
+  //   React.createElement(Pet, {
+  //     name: "Pogo",
+  //     animal: "Dog",
+  //     breed: "Dalmation"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Isabel",
+  //     animal: "Cat",
+  //     breed: "Persian"
+  //   })
+  // ]);
+
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me</h1>
+      <Pet name="Bandit" animal="dog" breed="lab" />
+      <Pet name="Luna" animal="dog" breed="lab" />
+      <Pet name="Isabel" animal="cat" breed="persian" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
