b73468f97f218695cd63247899905cc0ee6e6cea
dislersd <dislersd@gmail.com>
2019-11-16 16:44:51 -0800

    configured babel and started building img carousel

--- src/Details.js
+++ src/Details.js
@@ -2,13 +2,10 @@ import React from "react";
 import pet from "@frontendmasters/pet";
 
 class Details extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    loading: true
+  };
 
-    this.state = {
-      loading: true
-    };
-  }
   componentDidMount() {
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
