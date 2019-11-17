92caefeab2c9d0ecb3358cbdd4663b2e961bfb73
dislersd <dislersd@gmail.com>
2019-11-17 14:32:51 -0800

    added logo animation

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,20 +1,19 @@
 import React, { useState, useLayoutEffect } from "react";
 import { Link } from "@reach/router";
-import { css } from "@emotion/core";
+import { css, keyframes } from "@emotion/core";
 import colors from "./colors";
 
-const color = "#455073";
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
 const NavBar = () => {
   const [padding, setPadding] = useState(15);
 
-  useLayoutEffect(() => {
-    padding > 60 ? setPadding(15) : null;
-  }, [padding]);
-
   return (
     <header
-      onClick={() => setPadding(padding + 15)}
       css={css`
         background-color: ${colors.secondary};
         padding: ${padding}px;
@@ -26,9 +25,10 @@ const NavBar = () => {
         aria-label="logo"
         css={css`
           font-size: 60px;
-
+          display: inline-block;
+          animation: 1s ${spin} linear infinite;
           &:hover {
-            text-decoration: underline;
+            animation: 0.3s ${spin} linear infinite reverse;
           }
         `}
       >
